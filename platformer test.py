41419a834277fc762ac5dc86c37d1262d5286876
Oscar O'Maonaigh <oscarmeaney792@gmail.com>
2026-09-21 18:11:46 +0100

    Added main file and player image

---
+++ platformer test.py
@@ -0,0 +1,37 @@
+#little platformer test
+import pygame
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+dt = 0
+
+playerPos = pygame.Vector2(screen.get_width()/2, screen.get_height()/2)
+
+
+
+
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("white")
+
+    # RENDER YOUR GAME HERE
+    
+    pygame.draw.circle(screen,"red",playerPos,30)
+    
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
